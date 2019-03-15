7a68c5a97fd8b2461e38695e8920a6788f9bf1c9
xuwei1988 <xuwei1988@users.noreply.github.com>
2019-03-15 09:58:06 +0800

    First useful Git Project

---
+++ Ti28035_Scope_1/Scope/ScoSend.c
@@ -0,0 +1,177 @@
+#include "ScoSend.h"
+#include "ScoDevice.h"
+
+Uint8 sendBuff[200];
+Uint8 sendTotlen;              //数组有效数据长度
+Uint8 sendPos;              //当前数组操作位置
+
+typedef struct {
+    Uint16 addHi;
+    Uint16 addLo;
+    Uint16 len;
+    Uint16 ftype;
+}Framehead;
+
+void ScoSend_init(void) {
+    sendTotlen = 0;
+    sendPos = 0;
+}
+
+
+/* 发送字节获取，此函数主要作为串口发送接口 --------------------------------------------------------*/
+bool ScoSend_byteGet(Uint8 *pdata) {
+    bool res = false;
+    /* step1.2: 根据机器当前状态，决定是否获取下一帧数据 */
+    switch(ScoDevice_getState()) {
+        case ScoDisconnect:
+        case ScoNameUploading:
+        case ScoEventUploading:
+        case ScoWaitSetting:{
+            if(sendTotlen && sendPos != sendTotlen) {
+                *pdata = sendBuff[sendPos++];
+                res = true;
+            }
+            /* step1.1: 如果数据已经发送完毕 */
+            if(sendPos == sendTotlen) {
+                sendPos = 0;
+                sendTotlen = 0;
+            }
+            break;
+        }
+		case ScoRunning: {
+			if (sendTotlen == 0) {
+				SciSend_realData();
+			}
+			if (sendTotlen && sendPos != sendTotlen) {
+			    Framehead *fh = (Framehead *)sendBuff;
+			    switch (fh->ftype) {
+                    case frameEvtDiscript: {
+                        *pdata = sendBuff[sendPos++];
+                        break;
+                    }
+                    case frameReal:
+                    case frameEvtData: {
+
+                        if (sendPos < 6) {
+                            *pdata = sendBuff[sendPos++];
+                        }
+                        else {
+                            if (sendPos % 2 ) {
+                                *pdata = (sendBuff[6 + (sendPos - 6) / 2] & 0x00FF);
+                            }
+                            else {
+                                *pdata = ((sendBuff[6 + (sendPos - 6) / 2] & 0xFF00) >> 8);
+                            }
+                            sendPos++;
+                        }
+                        break;
+                    }
+			    }
+
+				res = true;
+			}
+
+			if (sendPos == sendTotlen) {
+				sendPos = 0;
+				sendTotlen = 0;
+			}
+
+			break;
+		}
+    }
+    return res;
+}
+
+void ScoSend_basicImfor(void) {
+    sendPos = 0;
+    sendTotlen = 0;
+    sendBuff[sendTotlen++] = SCOPE_ADDHI;
+    sendBuff[sendTotlen++] = SCOPE_ADDLO;
+    sendTotlen += SciDevice_getBaseImfor(&sendBuff[2]);
+}
+
+void ScoSend_channelName(Uint8 id) {
+    sendPos = 0;
+    sendTotlen = 0;
+    sendBuff[sendTotlen++] = SCOPE_ADDHI;
+    sendBuff[sendTotlen++] = SCOPE_ADDLO;
+    sendTotlen += SciDevice_getChannelID(&sendBuff[2], id);
+}
+
+void ScoSend_eventImfor(Uint8 id) {
+    sendPos = 0;
+    sendTotlen = 0;
+    sendBuff[sendTotlen++] = SCOPE_ADDHI;
+    sendBuff[sendTotlen++] = SCOPE_ADDLO;
+
+    sendTotlen += SciDevice_getEventImfor(&sendBuff[2], id);
+}
+
+void SciSend_realData(void) {
+    Uint16 len = 0;
+    sendPos = 0;
+    sendTotlen = 0;
+
+    len = SciDevice_getRunningData(&sendBuff[2]);
+    if(len) {
+        sendBuff[sendTotlen++] = SCOPE_ADDHI;
+        sendBuff[sendTotlen++] = SCOPE_ADDLO;
+        sendTotlen += len;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Ti28035_Scope_1/main.c
@@ -0,0 +1,145 @@
+#include "ScoUser.h"
+#include "ScoDevice.h"
+#include "ScoSCI.h"
+
+int sin[200] = { 0,9,19,28,38,48,57,67,76,85,95,104,113,122,131,139,
+                 148,156,165,173,181,188,196,203,210,217,224,231,237,243,249,254,
+                 260,265,269,274,278,282,286,289,292,295,298,300,302,304,305,306,
+                 307,307,308,307,307,306,305,304,302,300,298,295,292,289,286,282,
+                 278,274,269,265,260,254,249,243,237,231,224,217,210,203,196,188,
+                 181,173,165,156,148,139,131,122,113,104,95,85,76,67,57,48,
+                 38,28,19,9,0,-9,-19,-28,-38,-48,-57,-67,-76,-85,-95,-104,
+                 -113,-122,-131,-139,-148,-156,-165,-173,-181,-188,-196,-203,-210,-217,-224,-231,
+                 -237,-243,-249,-254,-260,-265,-269,-274,-278,-282,-286,-289,-292,-295,-298,-300,
+                 -302,-304,-305,-306,-307,-307,-308,-307,-307,-306,-305,-304,-302,-300,-298,-295,
+                 -292,-289,-286,-282,-278,-274,-269,-265,-260,-254,-249,-243,-237,-231,-224,-217,
+                 -210,-203,-196,-188,-181,-173,-165,-156,-148,-139,-131,-122,-113,-104,-95,-85,
+                 -76,-67,-57,-48,-38,-28,-19,-9};
+
+static interrupt void cpu_timer0_isr(void){
+
+    ScoSCI_recv();
+    ScoSCI_send();
+
+    static int16 cnt = 0;
+    static int16 cnt1 = 49;
+    static int16 cnt2 = 99;
+
+    if(cnt < 200) {
+        scoUserbuff[0] = sin[cnt++];
+        scoUserbuff[1] = sin[cnt1++];
+        scoUserbuff[2] = sin[cnt2++];
+        if(cnt == 200)
+            cnt = 0;
+        if(cnt1 == 200)
+            cnt1 = 0;
+        if(cnt2 == 200)
+            cnt2 = 0;
+    }
+
+    ScoDevice_refresh();
+
+
+
+    static Uint32 loop = 0;
+    if(loop++ == 30000) {
+        loop = 0;
+        static int n = 0;
+        ScoDevice_eventHappen(n++ % 3);
+    }
+
+    static int loop1 = 0;
+    if(loop1++ == 9) {
+        loop1 = 0;
+        ScoUser_eventPro();
+    }
+
+    PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
+}
+
+
+void main(void) {
+
+    InitSysCtrl();
+    DINT;
+    InitPieCtrl();
+    IER = 0x0000;
+    IFR = 0x0000;
+
+    /* 这里允许中断 －－*/
+    PieCtrlRegs.PIECTRL.bit.ENPIE = 1;
+
+    EALLOW;
+    /* Timer0 中断 －－*/
+    PieVectTable.TINT0 = &cpu_timer0_isr;
+    EDIS;
+
+    InitCpuTimers();
+
+    /* step1: 初始化各相关模块 */
+    ScoUser_init();
+    ScoDevice_init();
+    ScoSCI_init();
+
+
+    ConfigCpuTimer(&CpuTimer0, 60, 100);      //100us
+    CpuTimer0Regs.TCR.all = 0x4001;
+
+    PieCtrlRegs.PIEIER1.bit.INTx7 = 1;
+
+    IER |= M_INT1;
+    EINT;
+    ERTM;
+
+    /* step2: 输入设备基本信息 */
+    ScoDevice_setName("A Phrase V");
+    ScoDevice_setName("B Phrase V");
+    ScoDevice_setName("C Phrase V");
+
+    ScoDevice_setEvent(A_PHRASE_HIGH, CUSTOMER, 1, 1, SCOPE_CHN0);
+    ScoDevice_setEvent(B_PHRASE_LOW, WARNING, 1, 1, SCOPE_CHN0|SCOPE_CHN1);
+    ScoDevice_setEvent(C_PHRASE_HIGH, ERROR, 1, 1, SCOPE_CHN0|SCOPE_CHN1|SCOPE_CHN2);
+
+    /* step3: 检查设备相关设置是否正确 */
+    ScoDevice_check();
+
+    while(1) {
+       ScoDevice_handle();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
